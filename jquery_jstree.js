688d0e2614c13a2c94299c8d39b21d9e9112771c
Pierre Buyle <pierre@buyle.org>
2011-07-19 13:44:49 +0200

    * Use context and settings parameters in JavaScript behavior. * Add sync. of tree node selection as hidden input in parent from (if any). * JsTrees are now inputelemetn with support #ajax. * Use ibraries_get_path to retrieve  library path and store result i na variable to avoid re-scanning libraries directories at each page request.

--- jquery_jstree.js
+++ jquery_jstree.js
@@ -2,25 +2,86 @@
   Drupal.behaviors.jstree = {
     attach: function (context, settings) {
       // Initialize any jsTree defined from PHP
-      if ($.isPlainObject(Drupal.settings.jstree)) {
-        $.each(Drupal.settings.jstree, function (id, options) {
-            if (Drupal.settings.asOwnProperty(id)) {
-            // Build the tree
-            var tree = $('#'+id).jstree(options);
-            // And reset meta-data
-            tree.data(options.metadata);
-            // Store the tree element ID
-            Drupal.behaviors.jstree.tree_ids.push('#'+id);
-          }
+      if ($.isPlainObject(settings.jstree)) {
+        $.each(settings.jstree, function (id, options) {
+            if (settings.jstree.hasOwnProperty(id)) {
+              // Retrieve the tree element
+              var tree = $('#'+id, context);
+              if (tree.length) {
+                // Pre-process options
+                $.each(options, function(plugin, plugin_options) {
+                  if (options.hasOwnProperty(plugin)) {
+                    // Support URL pattern from tree/node data in plugins's ajax configuration
+                    if (plugin_options.hasOwnProperty('ajax') && plugin_options.ajax.hasOwnProperty('pattern')) {
+                      var pattern = decodeURI(plugin_options.ajax.pattern);
+                      var patternMatcher = new RegExp('{(.*?)}', 'g');
+                      plugin_options.ajax.url = function(node) {
+                        var dataSource = (node !== -1) ? $(node) : tree;
+                        return pattern.replace(patternMatcher, function(match, name){
+                          return dataSource.data(name) || '';
+                        });
+                      };
+                    }
+                  }
+                });
+                // Build the tree
+                tree.jstree(options);
+                // And reset meta-data
+                tree.data(options.metadata);
+                // Store the tree element ID
+                Drupal.behaviors.jstree.tree_ids.push('#'+id);
+                
+                var form = tree.parents('form');
+                if (form.length) {
+                 // Sync. tree-node selection to hidden fields in parent form. 
+                  var inputName = tree.data('name');
+                  tree.bind('select_node.jstree deselect_node.jstree', function(e, data) {
+                    console.log('sync.');
+                    form.find('input.selected-node').remove();
+                    tree.jstree('get_selected').each(function(index, node) {
+                      $.each($(node).data(), function(name, value) {
+                        $('<input></input>').attr({
+                          type: 'hidden',
+                          name: inputName + '[' + index + '][' + name + ']',
+                          value: value,
+                          'class': 'selected-node'
+                        }).appendTo(form);
+                      });
+                    });
+                  });
+                  // Ensure that our sync. handler is always the first one
+                  var events = tree.data('events');
+                  var handlers = events['select_node'];
+                  handlers.splice(0, 0, handlers.pop());
+                  handlers = events['deselect_node'];
+                  handlers.splice(0, 0, handlers.pop());
+                  
+                  // Serialize the parent form when the tree triggers an ajax event
+                  if (typeof Drupal.ajax === 'function' && Drupal.ajax.hasOwnProperty(id)) {
+                    Drupal.ajax[id].beforeSerialize = function(element, options) {
+                      console.log('beforeSerialize');
+                      $.each(form.serializeArray(), function(id, param) {
+                        options.data[param.name] = param.value;
+                      });
+                      Drupal.ajax.prototype.beforeSerialize.apply(this, arguments);
+                    };
+                  }
+                }
+              }
+            }
         });
       }
     },
     detach: function (context) {
       // Destroy any (automatically created) jsTree in the detached context
-      $(context, Drupal.behaviors.jstree.tree_ids.join(', ')).each(function(){
-        $(this).jstree('destroy');
-        // TODO remove $(this).attr('id') from Drupal.behaviors.jstree.tree_ids
-      });
+      if ($.isPlainObject(Drupal.settings.jstree)) {
+        $.each(Drupal.settings.jstree, function (id, options) {
+          if (Drupal.settings.jstree.hasOwnProperty(id)) {
+            var tree = $('#'+id, context);
+            tree.jstree('destroy');
+          }
+        });
+      };
     },
     tree_ids: []
   };
